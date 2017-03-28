d3e46afab09b09a8dcde7cdeea6eb80255e2b3c1
qibaohong <1286162711@qq.com>
2017-03-28 16:15:34 +0800

    祁保宏

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -8,20 +8,27 @@ angular.module('cyzApp')
 				
 			}).then(function(d){
 				$scope.a = d.data;
+				$scope.pageNum = 1;
+				$scope.total = $scope.a.length;
+				$scope.pageSize = 10;
+				$scope.totalPage = Math.ceil($scope.total/$scope.pageSize);
+				$scope.start = $scope.pageSize*($scope.pageNum - 1);
+				$scope.end = $scope.pageSize*$scope.pageNum;
+				$scope.a = $scope.a.slice($scope.start,$scope.end);
 			})
 		}
 //  管理层
         $scope.zz=""
 		$scope.guanli=function(){
+			
 			$http({
 				url:"http://47.88.16.225:402/users",
 				method:"get",
 				
 			}).then(function(d){
-
 				$scope.a=[]
 				for(var i=0;i<d.data.length;i++){
-						if(d.data[i].isadmin=='1'){
+						if(d.data[i].isadmin == '1'){
 							$scope.a.push(d.data[i])
 							$scope.zz="管理者"
 //							console.log($scope.a)
@@ -41,32 +48,35 @@ angular.module('cyzApp')
 			}).then(function(d){
 				$scope.a=[]
 				for(var i=0;i<d.data.length;i++){
-						if(d.data[i].isadmin!=='1'){
-							$scope.a.push(d.data[i])	
+						if(d.data[i].isadmin !== "1"){
+							$scope.a.push(d.data[i])
+							
 							$scope.zz="员工"
 //							console.log($scope.a)
 						}
 						
 				}
-				console.log(d.data)
 			},function(){
 				alert("error!")
 			})
 		}	
 				
 //  删除
+		var aid='';
 		$scope.del=function(id){
-
+			aid=id
+			
+		}
+		$scope.qd=function(){
 			$http({
-					url:"http://47.88.16.225:402/users/"+id,
+					url:"http://47.88.16.225:402/users/"+aid,
 					method:"delete"
 			}).then(function(d){
-					console.log(d)
+//					console.log(d)
 //					alert('删除成功!')			
 					Suser()
 			})
 		}
-	
 	
 //  修改	
 	$scope.xg=function(id){
@@ -119,10 +129,60 @@ angular.module('cyzApp')
 				method:"get",
 				
 			}).then(function(d){
+				
 				$scope.a = d.data;
-				console.log($scope.a.length)
+//				console.log($scope.a.length)
+				
+				$scope.a = d.data;
+				$scope.pageNum++;
+				if($scope.pageNum>$scope.totalPage){
+					$scope.pageNum=$scope.totalPage
+				}
+				$scope.total = $scope.a.length;
+				$scope.pageSize = 10;
+				$scope.totalPage = Math.ceil($scope.total/$scope.pageSize);
+				$scope.start = $scope.pageSize*($scope.pageNum - 1);
+				$scope.end = $scope.pageSize*$scope.pageNum;
+				$scope.a = $scope.a.slice($scope.start,$scope.end);
+				
 			})
 	}
+	
+//上一页
+		$scope.prev=function(){
+		$http({
+				url:"http://47.88.16.225:402/users",
+				method:"get",
+				
+			}).then(function(d){
+				$scope.a = d.data;
+//				console.log($scope.a.length)
+				
+				$scope.a = d.data;
+				$scope.pageNum--;
+				if($scope.pageNum<1){
+					$scope.pageNum=1
+				}
+				$scope.total = $scope.a.length;
+				$scope.pageSize = 10;
+				$scope.totalPage = Math.ceil($scope.total/$scope.pageSize);
+				$scope.start = $scope.pageSize*($scope.pageNum - 1);
+				$scope.end = $scope.pageSize*$scope.pageNum;
+				$scope.a = $scope.a.slice($scope.start,$scope.end);
+
+				
+			})
+	}
+		//封装
+		
+//		function fenye(sjson){
+//				$scope.total = sjson.length;
+//				$scope.pageSize = 10;
+//				$scope.totalPage = Math.ceil($scope.total/$scope.pageSize);
+//				$scope.start = $scope.pageSize*($scope.pageNum - 1);
+//				$scope.end = $scope.pageSize*$scope.pageNum;
+//				$scope.a = sjson.slice($scope.start,$scope.end);
+//		}
  	}])
  	
  	
